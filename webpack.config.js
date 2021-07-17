3654f7c047c9e3a1bcda05243b845974c532ddba
robin-hwang <robin.hwang@kakaocorp.com>
2021-07-17 15:23:00 +0900

    Fix custom-webpack Issue
    https://webpack.js.org/loaders/postcss-loader
    https://github.com/just-jeb/angular-builders/issues/810

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,22 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'postcss-loader',
-        options: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: () => [
-            require('postcss-import'),
-            require('tailwindcss'),
-            require('autoprefixer'),
-          ]
-        }
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              // ident: 'postcss',
+              // syntax: 'postcss-scss',
+              postcssOptions: {
+                plugins: [
+                  'postcss-import',
+                  'tailwindcss',
+                  'autoprefixer',
+                ]
+              }
+            }
+          }
+        ],
       }
     ]
   }
